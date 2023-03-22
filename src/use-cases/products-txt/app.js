0de5b1be264e66c7b817b488f64b36949ae271a1
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-22 17:25:37 -0500

    Insert product in TXT with XML entry done

--- src/use-cases/products-txt/app.js
+++ src/use-cases/products-txt/app.js
@@ -11,32 +11,20 @@ const { encrypt, decrypt } = require("../../functions/app");
 
 //Get CRUD use cases
 const addProductTxt = require("./insert-product-txt");
-//const selectProduct = require("./select-product");
-//const updateProduct = require("./update-product");
-//const deleteProduct = require("./delete-product");
+
 // #########
 //Relate CRUD use cases with Business Logic
 const addProductsTxt = addProductTxt({ productsDb, createProducts });
-//const selectProducts = selectProduct({ productsDb, decrypt });
-const selectProducts = null;
-//const updateProducts = updateProduct({ productsDb, patchProducts });
-//const deleteProducts = deleteProduct({ productsDb });
+
 
 // #########
 //Export use cases
 const services = Object.freeze({
   addProductsTxt,
-  //selectProducts
-  selectProducts,
-  //updateProducts,
-  //deleteProducts,
 });
 
 module.exports = services;
 
 module.exports = {
   addProductsTxt,
-  selectProducts,
-  //updateProducts,
-  //deleteProducts,
 };
